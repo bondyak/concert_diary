daf98322a6db9822356c79618a022d59b87877e0
Raghu Betina <raghu@firstdraft.com>
2016-12-06 02:40:55 +0000

    Whitelist columns in Concert for ActiveAdmin

--- app/admin/concert.rb
+++ app/admin/concert.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Concert do
 
+ permit_params :date, :notes, :artist_id, :venue_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
